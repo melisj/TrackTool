9f5d0c9de34ca59ad53c7b4930077fd876f1812c
johan han <Methaan123@gmail.com>
2019-11-17 17:09:12 +0100

    Added some post processing and added an extra pipe model into the tunnel.
    Added some lighting and created a small script to follow an object.

---
+++ Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+
+    public GameObject obj;
+
+    void Update()
+    {
+        Vector3 diff = obj.transform.position - Camera.main.transform.position;
+
+       Camera.main.transform.rotation = Quaternion.LookRotation(diff);
+    }
+}
